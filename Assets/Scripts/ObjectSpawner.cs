fa38ced7dd3ba9e5288a6ae191ba818fd6ba8a01
Azazelos <141460247+AzazelGuy@users.noreply.github.com>
2026-09-04 12:01:56 -0300

    Adicionado Base da mecanica de criação de objetos
    Aqui foi criado o script "ObjectSpawner" responsavel por criar as peças para serem utilizadas durante a gameplay

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -4,4 +4,13 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> ObjectstoSpawn;
+    private int indice = 0;
+    private void Start()
+    {
+        foreach (GameObject obj in ObjectstoSpawn)
+        {
+            GameObject curobjt = Instantiate(obj,new Vector3(transform.position.y + (10f * indice),transform.position.y,transform.position.z), Quaternion.identity);
+            indice++;
+        }
+    }
 }
